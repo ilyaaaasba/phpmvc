1e929b6725ca0e706401ec68e645da6b9949942e
ilyasbayuaji <bayu_173040156@mail.unpas.ac.id>
2019-10-06 02:50:23 -0700

    tugas mvc final

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,9 +1,26 @@
-<?php
-class About {
-    public function index($nama = 'Ilyas', $kota = 'Cimahi') {
-        echo "Halo nama saya $nama, saya tinggal di $kota"; 
+<?php 
+
+class About extends Controller {
+    public function index($nama = 'Ilyas', $pekerjaan = 'Asmen', $umur = 21)
+    {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
-    public function page() {
-        echo 'About/page';
+
+    public function page()
+    {
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
+
+
+
+
